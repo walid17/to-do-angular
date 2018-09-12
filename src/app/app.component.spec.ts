add9155f4c1590791ba001a3f6784784fc443205
walid17 <walidbenamor17@yahoo.fr>
2018-09-12 10:45:10 +0100

    todo app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';3
+import { FormsModule } from '@angular/forms';
+import { Todo } from 'src/app/todo';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
+ 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'todo-app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todo-app');
+
+  it(`should have a newTodo todo`, async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.newTodo instanceof Todo).toBeTruthy()
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  it('should display "Todos" in h1 tag', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-app!');
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from 'src/app/todo-data.service';
+import { Todo } from 'src/app/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'todo-app';
+  newTodo: Todo = new Todo();
+ constructor (private todoDataService: TodoDataService){
+
+ }
+ toggleTodoComplete(todo) {
+  this.todoDataService.toggleTodoComplete(todo);
+}
+addTodo() {
+  this.todoDataService.addTodo(this.newTodo);
+  this.newTodo = new Todo();
+}
+
+removeTodo(todo) {
+  this.todoDataService.deleteTodoById(todo.id);
+}
+
+get todos() {
+  return this.todoDataService.getAllTodos();
+}
 }
